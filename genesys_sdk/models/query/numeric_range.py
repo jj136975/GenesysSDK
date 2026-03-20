ea74127ed4b7701182a251f2668b9f7775190f2b
jj136975 <mattdaumas@hotmail.com>
2026-03-20 12:07:36 +0100

    fix: numeric range now take nullable arguments

--- genesys_sdk/models/query/numeric_range.py
+++ genesys_sdk/models/query/numeric_range.py
@@ -4,10 +4,10 @@ from serde import serde
 @serde(rename_all='camelcase')
 class NumericRange:
     # Greater than
-    gt: float = 0.0
+    gt: float | None = None
     # Greater than or equal to
-    gte: float = 0.0
+    gte: float | None = None
     # Less than
-    lt: float = 0.0
+    lt: float | None = None
     # Less than or equal to
-    lte: float = 0.0
+    lte: float | None = None
